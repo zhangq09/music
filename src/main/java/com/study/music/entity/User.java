4250d457adf994d3b949883ec63fbfbce7284ddd
zhangq09 <zq969811@gami.com>
2022-02-11 13:22:33 +0800

    fix Use roles to FetchType.EAGER

--- src/main/java/com/study/music/entity/User.java
+++ src/main/java/com/study/music/entity/User.java
@@ -19,7 +19,7 @@ public class User extends AbstractEntity implements UserDetails {
 
     private String nickname;
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
     private List<Role> roles;
 
